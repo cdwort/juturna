6d7de99543cf9ba62d515c51e54f8c497a3489cc
Amy Unger <amy.e.unger@gmail.com>
2014-04-07 17:58:00 -0500

    Test annual-investment

--- test/juturna/core_test.clj
+++ test/juturna/core_test.clj
@@ -5,3 +5,10 @@
 
 (fact "default current age is set correctly"
   default-current-age => 27)
+
+(deftest testing-annual-investment
+  (fact "it computes reasonable investment rates"
+    (annual-investment 25000 0.2) => 5000.0)
+  (fact "it handles negative investment rates"
+    (annual-investment 25000 -0.1) => -2500.0)
+)
